8a5c4083dc651aba954def9c0e92646b916e95bc
Ehsan Langroudi (kendrick) <ehsanlangroudi.kk@gmail.com>
2022-06-30 14:03:15 +0430

    new game function and showing ahead team on score

--- scrimba-score-counter/index.js
+++ scrimba-score-counter/index.js
@@ -4,10 +4,26 @@ let team2 = document.getElementById('team2')
 
 console.log(parseInt(team1) + 1)
 
-function addScoreT1(addedScore){ team1.textContent = parseInt(team1.innerHTML) + addedScore; console.log(team1)}
+function addScoreT1(addedScore){ team1.textContent = parseInt(team1.innerHTML) + addedScore; console.log(team1)
+    if (parseInt(team1.innerHTML) > parseInt(team2.innerHTML)){
+        team1.classList.add('ahead')
+        team2.classList.remove('ahead')
+    }
+}
 
-function addScoreT2(addedScore){ team2.textContent = parseInt(team2.innerHTML) + addedScore; console.log(team2)}
+function addScoreT2(addedScore){ team2.textContent = parseInt(team2.innerHTML) + addedScore; console.log(team2)
+    if (parseInt(team1.innerHTML) < parseInt(team2.innerHTML)){
+        team2.classList.add('ahead')
+        team1.classList.remove('ahead')
+    }
 
+}
 
+function newGame() {
+    team1.textContent=0
+    team2.textContent=0
+    team1.classList.remove('ahead')
+    team2.classList.remove('ahead')
+}
 
 
